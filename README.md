b0c8d7b3265fbae10360fc7b7ac19520243f350e
Ian Vanagas <34755028+ivanagas@users.noreply.github.com>
2022-03-05 23:33:12 +0000

    update readme and match_size arg

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ https://discord.com/api/oauth2/authorize?client_id=829578421893070849&permission
 # join link (dev):
 https://discord.com/api/oauth2/authorize?client_id=948647613652172891&permissions=68672&scope=bot
 
+# demo recording
+https://www.loom.com/share/0c447690d10e41aaa47644e8b83bf2bb
+
 # Permissions needed:
 - scopes: bot
 - bot permissions: Read Messages/View Channels, Send Messages, Read Message History, Add Reactions
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 from discord.ext import commands
 from discord.utils import find
 from discord.ext.commands import has_permissions
+from discord.ext import commands
 
 
 load_dotenv()
@@ -44,9 +45,13 @@ async def start(ctx):
 
 @bot.command(name='match', help='Runs matching and sends messages with pairs')
 @has_permissions(use_slash_commands=True)
-async def match(ctx):
+async def match(ctx, match_size=2):
 
-    last_message = await ctx.message.channel.history().find(lambda m: (m.author.id == bot.user.id) and (m.content == 'React to this message to be matched'))
+    if match_size <= 1:
+        await ctx.send('The match size must be an integer greater than 1. For example "/m match 3"')
+        return
+
+    last_message = await ctx.message.channel.history(limit=None).find(lambda m: (m.author.id == bot.user.id) and (m.content == 'React to this message to be matched'))
     if last_message == None:
         await ctx.send('Start message not found, please use "/m start" to gather users for matches.')
         return
@@ -57,20 +62,38 @@ async def match(ctx):
             if user.id != bot.user.id and user.id not in match_list:
                 match_list.append(user.id)
     
-    if len(match_list) <= 1:
-        await ctx.send('You need at least 2 people to react to create a match.')
+    if len(match_list) < match_size:
+        await ctx.send(f'You need at least {match_size} people to react to create a match.')
         return
 
     random.shuffle(match_list)
-    pairs = ([match_list[i:i + 2] for i in range(0, len(match_list), 2)])
+    pairs = ([match_list[i:i + match_size] for i in range(0, len(match_list), match_size)])
     message = await ctx.send('Here are the matches:')
-    for match in pairs:
-        if len(match) == 2:
-            await message.channel.send(f"<@{match[0]}> & <@{match[1]}>")
-            continue
-        await message.channel.send(f"<@{match[0]}> is left out :(")
-    emoji = "😀"
-    await message.channel.send("Message your match to get to know them better {}".format(emoji))
     
+    for match in pairs:
+        if len(match) == match_size:
+            match_msg = f"<@{match[0]}>"
+            for matched_user in match[1:]:
+                match_msg += f" & <@{matched_user}>"
+            await message.channel.send(match_msg)
+            continue
+
+        if len(match) == 1:
+            await message.channel.send(f"<@{match[0]}> is left out :(")
+            continue
+        
+        left_msg = f"<@{match[0]}>"
+        for left_user in match:
+            left_msg += f" & <@{left_user}>"
+        left_msg += " are left out :("
+        await message.channel.send(left_msg)
+    
+    emoji = "😀"
+    if match_size == 2:
+        await message.channel.send("Message your match to get to know them better {}".format(emoji))
+        return
+    await message.channel.send("Message your matches to get to know them better {}".format(emoji))
+
+
 bot.run(TOKEN)
 
